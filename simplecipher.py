f4576110373d8b188003f770396ac94d556ce44c
OMEGA-TSC <76587686+OMEGA-TSC@users.noreply.github.com>
2024-01-26 07:40:35 +0100

    Add files via upload

--- simplecipher.py
+++ simplecipher.py
@@ -1,18 +1,27 @@
-#Library made by Vilém Urbánek
 class encript:
 
     def __init__(self): 
         hex_ch = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
         self.hex_ch = hex_ch
         
-    def hex(self, data):
+    def hex(self, text: str):
         ch = []
-        self.data = data
-        for c in self.data:
-            ch_num = ord(c)
-            ch.append(int(ch_num / 16))
-            ch.append(ch_num % 16)
+        for c in text:
+            ch.append(int(ord(c) / 16))
+            ch.append(ord(c) % 16)
         res = "".join(self.hex_ch[ch[i]] for i in range(len(ch)))
+        return res
+    
+    def caesar(self, text: str, shift: int):
+        res = ""
+        for i in range(len(text)):
+            char = text[i]
+            if char.isupper():
+                res += chr((ord(char) + shift - 65) % 26 + 65)
+            elif char.islower():
+                res += chr((ord(char) + shift - 97) % 26 + 97)
+            else:
+                res += char
         return res
     
 class decrypt:
@@ -21,13 +30,22 @@ class decrypt:
         hex_ch = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
         self.hex_ch = hex_ch
 
-    def hex(self, data):
-        ch = []
+    def hex(self, text: str):
         n = []
-        self.data = data
-        for c in self.data:
+        for c in text:
             n.append(self.hex_ch.index(c))
-        for i in range(0,len(n),2):
-            ch.append(((n[i] * 16) + n[i+1]))
-        res = "".join(chr(ch[i]) for i in range(len(ch)))
+        res = "".join(chr(((n[i] * 16) + n[i+1])) for i in range(0,len(n),2))
+        return res
+    
+    def caesar(self, text: str, shift: int):
+        shift = (shift - (shift * 2))
+        res = ""
+        for i in range(len(text)):
+            char = text[i]
+            if char.isupper():
+                res += chr((ord(char) + shift - 65) % 26 + 65)
+            elif char.islower():
+                res += chr((ord(char) + shift - 97) % 26 + 97)
+            else:
+                res += char
         return res
